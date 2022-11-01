3ebea5da5dff4bbc170e1be91548161b4bc1d261
Fabio de Moura Martins <fabio.mmouras@hotmail.com>
2022-11-01 14:29:11 -0300

    CoffeeCartOrder Component
    Component of the order completion page

---
+++ src/pages/CompleteOrder/components/CoffeeCartCard/styles.ts
@@ -0,0 +1,66 @@
+import { toColorString } from 'polished';
+import styled from 'styled-components';
+
+export const CoffeeCartCardContainer = styled.div`
+    width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+
+    border-bottom: 1px solid ${({ theme }) => theme.colors['base-button']};
+    padding-bottom: 1.5rem;
+    margin-bottom: 1.5rem;
+
+    > div {
+        display: flex;
+        align-items: center;
+        gap: 1.25rem;
+
+        img {
+            width: 4rem;
+            height: 4rem;
+        }
+    }
+
+    > p {
+        font-weight: 700;
+        align-self: flex-start;
+    }
+`;
+
+export const ActionsContainer = styled.div`
+    margin-top: 0.5rem;
+    height: 2rem;
+    display: flex;
+    align-items: center;
+    gap: 0.5rem;
+
+    > div {
+        max-width: 4.5rem;
+        height: 100%;
+    }
+`;
+
+export const RemoveButton = styled.button`
+    height: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 0.25rem;
+    color: ${({ theme }) => theme.colors['base-text']};
+    text-transform: uppercase;
+    font-size: 0.75rem;
+    border: none;
+    background: ${({ theme }) => theme.colors['base-button']};
+    padding: 0 0.5rem;
+    border-radius: 6px;
+    transition: 0.4s;
+
+    &:hover {
+        background: ${({ theme }) => theme.colors['base-hover']};
+    }
+
+    svg {
+        color: ${({ theme }) => theme.colors['brand-purple']};
+    }
+`;
