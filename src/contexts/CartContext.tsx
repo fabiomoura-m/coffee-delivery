f0d6f5a2f9fc6e3a10f8314c3bf8e7221d6dc5a1
Fabio de Moura Martins <fabio.mmouras@hotmail.com>
2022-11-03 15:12:06 -0300

    feat: dynamically updated total order price

--- src/contexts/CartContext.tsx
+++ src/contexts/CartContext.tsx
@@ -9,6 +9,7 @@ export interface CartItem extends Coffee {
 interface CartContextType {
     cartItems: CartItem[];
     cartQuantity: number;
+    cartItemsTotal: number;
     addCoffeeToCart: (coffee: CartItem) => void;
     changeCartItemQuantity: (
         cartItemId: number,
@@ -26,6 +27,9 @@ export function CartContextProvider({ children }: CartContextProviderProps) {
     const [cartItems, setCartItems] = useState<CartItem[]>([]);
 
     const cartQuantity = cartItems.length;
+    const cartItemsTotal = cartItems.reduce((total, cartItem) => {
+        return total + cartItem.price * cartItem.quantity;
+    }, 0);
 
     function addCoffeeToCart(coffee: CartItem) {
         const coffeeAlreadyExistsInCart = cartItems.findIndex(
@@ -83,7 +87,8 @@ export function CartContextProvider({ children }: CartContextProviderProps) {
                 cartQuantity,
                 addCoffeeToCart,
                 changeCartItemQuantity,
-                removeCartItem
+                removeCartItem,
+                cartItemsTotal
             }}
         >
             {children}
