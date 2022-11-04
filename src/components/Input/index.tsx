e9972d8f53a80d1354c53ffeb3fcb193854a12f6
Fabio de Moura Martins <fabio.mmouras@hotmail.com>
2022-11-04 09:50:58 -0300

    style: put optional in complement input

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,16 +1,25 @@
 import { forwardRef, InputHTMLAttributes } from 'react';
 import { RegularText } from '../Typography';
-import { InputStyleContainer, InputWrapper } from './styles';
+import {
+    InputStyleContainer,
+    InputStyled,
+    InputWrapper,
+    RightText
+} from './styles';
 
 type InputProps = InputHTMLAttributes<HTMLInputElement> & {
     error?: string;
+    rightText?: string;
 };
 
 export const Input = forwardRef<HTMLInputElement, InputProps>(
-    ({ error, className, ...props }, ref) => {
+    ({ error, className, rightText, ...props }, ref) => {
         return (
             <InputWrapper className={className}>
-                <InputStyleContainer {...props} ref={ref} />
+                <InputStyleContainer>
+                    <InputStyled {...props} ref={ref} />
+                    {rightText && <RightText>{rightText}</RightText>}
+                </InputStyleContainer>
                 {error && <RegularText size="s">{error}</RegularText>}
             </InputWrapper>
         );
--- src/components/Input/styles.ts
+++ src/components/Input/styles.ts
@@ -11,17 +11,27 @@ export const InputWrapper = styled.div`
     }
 `;
 
-export const InputStyleContainer = styled.input`
+export const InputStyleContainer = styled.div`
     height: 2.625rem;
     border-radius: 4px;
     border: 1px solid ${({ theme }) => theme.colors['base-button']};
     background: ${({ theme }) => theme.colors['base-input']};
-    transition: 4s;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    transition: 0.4s;
+    overflow: hidden;
 
-    &:focus {
+    &:focus-within {
         border-color: ${({ theme }) => theme.colors['brand-yellow-dark']};
     }
+`;
 
+export const InputStyled = styled.input`
+    flex: 1;
+    background: none;
+    border: none;
+    height: 100%;
     color: ${({ theme }) => theme.colors['base-text']};
     font-size: 0.75rem;
     padding: 0 0.75rem;
@@ -29,4 +39,11 @@ export const InputStyleContainer = styled.input`
     &::placeholder {
         color: ${({ theme }) => theme.colors['base-label']};
     }
+`;
+
+export const RightText = styled.p`
+    font-size: 0.75rem;
+    margin-right: 0.75rem;
+    font-style: italic;
+    color: ${({ theme }) => theme.colors['base-label']};
 `;
