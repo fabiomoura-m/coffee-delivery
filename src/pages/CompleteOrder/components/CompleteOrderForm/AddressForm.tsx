7b788654a98b02bddc103603da1112b0ea782045
Fabio de Moura Martins <fabio.mmouras@hotmail.com>
2022-11-04 09:26:37 -0300

    feat: configure components for form validation

--- src/pages/CompleteOrder/components/CompleteOrderForm/AddressForm.tsx
+++ src/pages/CompleteOrder/components/CompleteOrderForm/AddressForm.tsx
@@ -1,10 +1,28 @@
 import { Input } from '../../../../components/Input';
 import { AddressFormContainer } from './styles';
+import { useFormContext } from 'react-hook-form';
+
+interface ErrosType {
+    errors: {
+        [key: string]: {
+            message: string;
+        };
+    };
+}
 
 export function AddressForm() {
+    const { register, formState } = useFormContext();
+
+    const { errors } = formState as unknown as ErrosType;
     return (
         <AddressFormContainer>
-            <Input placeholder="CEP" type="number" className="cep" />
+            <Input
+                placeholder="CEP"
+                type="number"
+                className="cep"
+                {...register('cep')}
+                error={errors.cep?.message}
+            />
             <Input placeholder="Rua" className="street" />
             <Input placeholder="Número" type="number" />
             <Input placeholder="Complemento" className="complement" />
